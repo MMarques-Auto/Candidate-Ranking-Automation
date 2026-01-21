1ee1ad2ff3a0b64e69747f00ea2ea4f6d136ae88
Murilo Marques <murilomarquesfigeira@gmail.com>
2026-01-21 20:59:55 -0300

    Atualização: Code rodando com csv externo e gerando filtros melhores, com criação de colunas e agrupamento

--- Headhunter/Headhunter.py
+++ Headhunter/Headhunter.py
@@ -1,12 +1,39 @@
 import pandas as pd
 
-df = pd.read_csv('candidatos.csv')
+#Iniciar com tratamento de dados, vamos limpar a planilha
+df = pd.read_csv('Candidatos.csv')
+df['Skill'] = df['Skill'].str.strip()
+df['Skill'] = df['Skill'].str.upper()
 
-filtro = (df['Experiencia_Anos'] >= 3) & (df['Linguagem'] == 'Python') & (df['Pretensao_Salarial'] <= 60000)
-bons_candidatos = df[filtro]
+df = df.dropna(subset=['Pretensao'])
 
-print("--- Candidatos No Orçamento ---")
-print(bons_candidatos[['Nome', 'Pretensao_Salarial']])
+df['Experiencia'] = df['Experiencia'].fillna(df['Experiencia'].median())
+#tratamento de dados concluido
 
-media = bons_candidatos['Pretensao_Salarial'].mean()
-print(f"\nA média de pretenção salarial dos candidatos bons é: £{media:.2f}")
+#criação da senioridade, separando juniors plenos e seniors
+def calc_nivel(anos):
+    if anos > 8:
+        return "Senior"
+    elif anos < 3:
+        return "Junior"
+    else:
+        return "Pleno"
+
+df['Nivel'] = df['Experiencia'].apply(calc_nivel)
+#fim da criação e implementação da senioridade
+
+#criando tabela agrupada por localização
+aprovados = df[df['Status_Entrevista'] == 'Aprovado']
+
+relatorio_final = aprovados.groupby('Localizacao').agg({
+    'Pretensao': 'mean',
+    'Experiencia': 'sum'
+})
+
+print(df)
+print("\n\n\n")
+print("--- Resultado do groupby ---")
+print(relatorio_final)
+
+relatorio_final.to_csv('Relatório_Candidatos_Europa;csv')
+print("\nRelatório exportado com sucesso!")
